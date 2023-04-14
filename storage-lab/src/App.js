fc20ba7586c75053386b2643da866259bac06af6
rtree <arakitomohiko@gmail.com>
2023-04-14 22:25:14 +0900

    update

--- storage-lab/src/App.js
+++ storage-lab/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { SimpleStorage } from "./abi/abi";
+import Web3 from "web3";
 import './App.css';
 
+// Access our wallet inside of our dapp
+const web3 = new Web3(Web3.givenProvider);
+// Contract address of the deployed smart contract
+const contractAddress = "0x0FBF8E4536f40831EE7eC35E2127D8a6580Bdee7";
+const storageContract = new web3.eth.Contract(SimpleStorage, contractAddress);
+
 function App() {
+  // Hold variables that will interact with our contract and frontend
+  const [number, setUint]   = useState(0);
+  const [getNumber, setGet] = useState("0");
+  
+  const numberSet = async (t) => {
+    t.preventDefault();
+    const accounts = await window.ethereum.enable();
+    const account = accounts[0];
+    // Get permission to access user funds to pay for gas fees
+    const gas = await storageContract.methods.set(number).estimateGas();
+    const post = await storageContract.methods.set(number).send({
+      from: account,
+      gas,
+    });
+  };
+
+  const numberGet = async (t) => {
+    t.preventDefault();
+    const post = await storageContract.methods.get().call();
+    setGet(post);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="main">
+       <div className="card">
+         <form className="form" onSubmit={numberSet}>
+           <label>
+             Set your uint256:
+             <input
+               className="input"
+               type="text"
+               name="name"
+               onChange={(t) => setUint(t.target.value)}
+             />
+           </label>
+           <button className="button" type="submit" value="Confirm">
+             Confirm
+           </button>
+         </form>
+         <br />
+         <button className="button" onClick={numberGet} type="button">
+           Get your uint256
+         </button>
+         {getNumber}
+       </div>
+     </div>
   );
 }
 
