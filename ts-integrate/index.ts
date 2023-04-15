154aaecd9df41f90458461f9515e93ee998ac165
rtree <arakitomohiko@gmail.com>
2023-04-15 17:32:09 +0900

    garbase collected failed attempts

--- ts-integrate/index.ts
+++ ts-integrate/index.ts
@@ -1,9 +1,16 @@
 import Web3 from 'web3'
 import { Web3Adapter } from '@safe-global/protocol-kit'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const { INFURA_API_KEY } = process.env;
 
-const provider = new Web3.providers.HttpProvider(INFURA_API_KEY)
-const web3 = new Web3(provider)
+if (!INFURA_API_KEY) {
+  throw new Error('INFURA_API_KEY is not defined in environment variables');
+}
+const provider  = new Web3.providers.HttpProvider(INFURA_API_KEY)
+const web3      = new Web3(provider)
 const safeOwner = '0x485A974140923524a74B0D72aF117852F31B412D'
 
 const ethAdapter = new Web3Adapter({
