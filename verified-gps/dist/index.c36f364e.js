f469c6e3434b2f29e61ec2c79a5782cfaa02943c
rtree <arakitomohiko@gmail.com>
2023-04-16 00:00:46 +0900

    p

---
+++ verified-gps/dist/index.c36f364e.js
@@ -0,0 +1,79 @@
+//import Web3 from 'web3'
+//import { Web3Adapter } from '@safe-global/protocol-kit'
+//import SafeApiKit from '@safe-global/api-kit'
+//import Safe, { SafeFactory } from '@safe-global/protocol-kit'
+//import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types'
+const txServiceUrl = "https://safe-transaction-goerli.safe.global/";
+async function main() {
+    // Check if MetaMask is installed
+    if (typeof window.ethereum === "undefined") {
+        alert("MetaMask is not installed. Please install MetaMask and try again.");
+        return;
+    }
+    // Request account access
+    await window.ethereum.request({
+        method: "eth_requestAccounts"
+    });
+    const web3 = new Web3(window.ethereum);
+    const [senderAddress] = await web3.eth.getAccounts();
+    const safeOwner = senderAddress;
+    const safeAddress = "0x0914566875dF1b7Fb6cb67c55058194D6a0616c3";
+    const ethAdapter = new Web3Adapter({
+        web3,
+        signerAddress: safeOwner
+    });
+    const safeService = new SafeApiKit({
+        txServiceUrl,
+        ethAdapter
+    });
+    const contractAddress = "0xCBE177C44Cff283701f82e69526677a896F1f4b1";
+    const param1 = "999999999";
+    const param2 = "9999999999";
+    const data = web3.eth.abi.encodeFunctionCall({
+        name: "set",
+        type: "function",
+        inputs: [
+            {
+                type: "uint256",
+                name: "latitude"
+            },
+            {
+                type: "uint256",
+                name: "longitude"
+            }
+        ]
+    }, [
+        param1,
+        param2
+    ]);
+    const safeTransactionData = {
+        to: contractAddress,
+        data,
+        value: "0"
+    };
+    const safeSdk = await Safe.create({
+        ethAdapter,
+        safeAddress: safeAddress
+    });
+    const safeTransaction = await safeSdk.createTransaction({
+        safeTransactionData
+    });
+    const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
+    const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
+    const proposedTxResponse = await safeService.proposeTransaction({
+        safeAddress,
+        safeTransactionData: safeTransaction.data,
+        safeTxHash,
+        senderAddress,
+        senderSignature: senderSignature.data,
+        origin
+    });
+    console.log("Transaction proposed:", proposedTxResponse);
+}
+document.getElementById("execute").addEventListener("click", ()=>{
+    main().catch((error)=>{
+        console.error("An error occurred:", error);
+    });
+});
+
+//# sourceMappingURL=index.c36f364e.js.map
