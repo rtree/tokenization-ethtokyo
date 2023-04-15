13b7ba43403befb37903cbb5604040f68cfc8f4a
rtree <arakitomohiko@gmail.com>
2023-04-15 21:46:58 +0900

    wondrting

--- ts-integrate/index.ts
+++ ts-integrate/index.ts
@@ -4,24 +4,18 @@ import dotenv from 'dotenv'
 import SafeApiKit from '@safe-global/api-kit'
 import Safe, { SafeFactory } from '@safe-global/protocol-kit'
 import { SafeTransactionDataPartial } from '@safe-global/safe-core-sdk-types'
-import PrivateKeyProvider from 'truffle-privatekey-provider';
 
 dotenv.config()
-const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
+const { INFURA_API_KEY } = process.env;
 
 if (!INFURA_API_KEY) {
   throw new Error('INFURA_API_KEY is not defined in environment variables');
 }
-if (!PRIVATE_KEY) {
-  throw new Error('PRIVATE_KEY is not defined in environment variables');
-}
-const provider = new PrivateKeyProvider(PRIVATE_KEY as string, `https://goerli.infura.io/v3/${INFURA_API_KEY}`);
+const provider     = new Web3.providers.HttpProvider(INFURA_API_KEY)
 const web3         = new Web3(provider)
 const safeOwner    = '0x485A974140923524a74B0D72aF117852F31B412D'
 const senderAddress = '0x485A974140923524a74B0D72aF117852F31B412D'
 const safeAddress  = '0x0914566875dF1b7Fb6cb67c55058194D6a0616c3'
-//const provider     = new Web3.providers.HttpProvider(INFURA_API_KEY)
-//const provider = new PrivateKeyProvider(PRIVATE_KEY, `https://goerli.infura.io/v3/${INFURA_API_KEY}`);
 
 const ethAdapter   = new Web3Adapter({
   web3,
@@ -59,17 +53,20 @@ async function main() {
   const safeTransaction = await safeSdk.createTransaction({ safeTransactionData });
 
   const safeTxHash = await safeSdk.getTransactionHash(safeTransaction);
-  const signatureObject = web3.eth.accounts.sign(safeTxHash, PRIVATE_KEY);
-  const senderSignature = signatureObject.signature;
-  //const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
+  const senderSignature = await safeSdk.signTransactionHash(safeTxHash);
   const proposedTxResponse = await safeService.proposeTransaction({
     safeAddress,
     safeTransactionData: safeTransaction.data,
     safeTxHash,
     senderAddress,
-    senderSignature,
+    senderSignature: senderSignature.data,
     origin
   });
+
+  // Sign the transaction
+  const signature = await safeSdk.signTransaction(safeTransaction);
+
+  // Propose the transaction
 
 
   console.log('Transaction proposed:', proposedTxResponse);
