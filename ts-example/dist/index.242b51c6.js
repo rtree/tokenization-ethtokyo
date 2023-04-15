497ead126ed91d0aa9dd11c370ccb07191b5364e
rtree <arakitomohiko@gmail.com>
2023-04-15 19:15:21 +0900

    p

---
+++ ts-example/dist/index.242b51c6.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const button = document.getElementById("myButton");
+    if (button) button.addEventListener("click", ()=>{
+        alert("Hello, TypeScript!");
+    });
+});
+
+//# sourceMappingURL=index.242b51c6.js.map
